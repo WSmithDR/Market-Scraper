303f39a6773669e3235b871345224cd22865f453
SmithDR <wsduenas@espol.edu.ec>
2025-11-27 18:03:48 -0500

    feat: resuelto problema de lectura de operadores

--- operations/__init__.py
+++ operations/__init__.py
@@ -1,6 +1,6 @@
 from .connect_to_mongodb import connect_to_mongodb
 from .setup_output_directory import setup_output_directory
-from .process_job_data import process_and_save_jobs
+from .process_and_save_jobs import process_and_save_jobs
 
 __all__ = [
     "connect_to_mongodb",
