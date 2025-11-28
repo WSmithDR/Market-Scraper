2ba3cb246a0ffb0201e11daf81f5a267ef71c603
SmithDR <wsduenas@espol.edu.ec>
2025-11-27 19:18:53 -0500

    fest: configurando buscador de google

--- operations/__init__.py
+++ operations/__init__.py
@@ -2,10 +2,12 @@ from .connect_to_mongodb import connect_to_mongodb
 from .setup_output_directory import setup_output_directory
 from .process_and_save_jobs import process_and_save_jobs
 from .hours_old import hours_old_since_2025
+from .safe_scrape_jobs import safe_scrape_jobs
 
 __all__ = [
     "connect_to_mongodb",
     "setup_output_directory",
     "process_and_save_jobs",
-    "hours_old_since_2025"
+    "hours_old_since_2025",
+    "safe_scrape_jobs"
 ]
